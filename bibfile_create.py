d4784bbc1b8dae079e982d16c70f18a45deaf4e7
bjasperson <ben.jasperson@gmail.com>
2023-12-15 14:29:48 -0600

    initial commit: bibfile_create and combined_bib

---
+++ bibfile_create.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from predNN import import_data
+
+def filter_df(df):
+    # repeat filtering as done for manuscript
+    df = df[df['lat'] == 'fcc'] # limit to fcc for now
+
+    # property list to explore, commented out props with few data points
+    matl_props = ['lattice_constant_fcc', 'bulk_modulus_fcc',
+            'c44_fcc', 'c12_fcc', 'c11_fcc',
+            'cohesive_energy_fcc', 
+            'thermal_expansion_coeff_fcc', 
+            'surface_energy_100_fcc', 
+            ]
+    
+    df = df.dropna(subset=matl_props)
+
+    return df
+
+def main():
+    # import list of models used
+    df = import_data()
+    df = filter_df(df)
+    models_list = df.model.drop_duplicates().to_list()
+
+    # combine into one bib
+    combined_bib = ""
+    for model in models_list:
+        kimcode = model.split("__")[1]
+        with open(f'./data/param_files/{model}/kimcite-{kimcode}.bib','r') as file:
+            lines = file.readlines()
+            for line in lines:
+                combined_bib += line
+            lines += "\n\n"
+
+    # save to folder
+    with open(f'./data/combined_bib.bib','w') as out:
+        out.write(combined_bib)
+
+
+    return
+
+
+if __name__ == "__main__":
+    main()
